c2ff6a5993646fc6903bdc08ad93a365bc89b300
gaeunji <jikhwn@gmail.com>
2025-09-17 17:32:08 +0900

    Week3 Update

--- Week-3/2.2.mergesort.py
+++ Week-3/2.2.mergesort.py
@@ -6,9 +6,36 @@ def merge(h: int, m: int, U: List[int], V: List[int], S: List[int]) -> None:
     
     i = j = k = 0
     # Complete the code here
+    # 요소들이 남아있는 동안 비교
+    while i < h and j < m:
+        if U[i] <= V[j]:
+            S[k] = U[i]
+            i += 1
+        else:
+            S[k] = V[j]
+            j += 1
+        k += 1
+    
+    # 남은 요소들을 S에 복사
+    while i < h:
+        S[k] = U[i]
+        i += 1
+        k += 1
+    while j < m:
+        S[k] = V[j]
+        j += 1
+        k += 1
 
+
+                
 def mergesort(n: int, S: List[int]) -> None:
     h = n // 2
     m = n - h
     if n > 1:
         # Complete the code here
+        U = S[:h]
+        V = S[h:]
+        mergesort(h, U)
+        mergesort(m, V)
+        merge(h, m, U, V, S)
+    
