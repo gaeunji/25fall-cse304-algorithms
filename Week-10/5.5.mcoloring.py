d696e3c1df3ad1b9467f9e03c7c010edb919be8b
gaeunji <jikhwn@gmail.com>
2025-11-24 18:13:23 +0900

    update week10

--- Week-10/5.5.mcoloring.py
+++ Week-10/5.5.mcoloring.py
@@ -9,14 +9,27 @@ count: int = 0              # Solution counter
 def promising(i: int) -> bool:
     global W, vcolor
     # Complete the code here
+    j = 1
+    while j < i:
+        if W[i][j] == 1 and vcolor[i] == vcolor[j]:
+            return False
+        j += 1
+    
 
     return True
 
-def mcoloring(i: int, m: int) -> None:
+def mcoloring(i: int, m: int) -> None: # (0, m)
     global n, vcolor, count
-    if promising(i):
+    if promising(i): # i는 현재 색칠된 정점의 수 (0으로 넘어옴)
         if i == n:
             print(vcolor[1:])
             # Complete the code here
+            count += 1 
         else:
             # Complete the code here
+            for j in range(1, m + 1):
+                vcolor[i + 1] = j
+                mcoloring(i + 1, m)
+    return
+
+            
--- Week-10/5.7.knapsack.0-1.bt.py
+++ Week-10/5.7.knapsack.0-1.bt.py
@@ -12,6 +12,19 @@ include: List[bool]
 def promising(i: int, weight: float, profit: float) -> bool:
     global n, W, w, p, maxprofit
     # Complete the code here
+    if weight >= W:
+        return False
+    else: # 무게 초과가 아닌 경우 -> 나머지 아이템들을 넣었을 때의 bound 계산
+        bound = profit
+        j = i + 1
+        totweight = weight
+        while j <= n and totweight + w[j] <= W:
+            totweight += w[j]
+            bound = bound + p[j]
+            j += 1
+        k = j
+        if k <= n:
+            bound = bound + (W - totweight) * p[k] / w[k]
 
     return bound > maxprofit
 
@@ -19,5 +32,12 @@ def knapsack(i: int, weight: float, profit: float) -> None:
     global n, W, w, p, bestset, include, maxprofit
     if weight <= W and profit > maxprofit:
         # Complete the code here
-    if promising(i, weight, profit):
+        maxprofit = profit
+        bestset = include[:]
+    if promising(i, weight, profit): # 다음 물건에 대해 포함하거나 포함하지 않거나 두 가지 경우 고려
         # Complete the code here
+        include [i + 1] = True
+        knapsack(i+1, weight + w[i+1], profit + p[i+1])
+        include [i + 1] = False
+        knapsack(i+1, weight, profit)
+    return 
