452505e8a3c15edff168cea517051dd449d0aa50
gaeunji <jikhwn@gmail.com>
2025-11-27 14:57:31 +0900

    update week 11 and 12

--- Week-12/7.1.insertion.sort.py
+++ Week-12/7.1.insertion.sort.py
@@ -2,3 +2,11 @@ from typing import List
 
 def insertionsort(n: int, S: List[int]) -> None:
     # Complete the code here
+    for i in range(1, n):
+        key = S[i]
+        j = i - 1
+        while j >= 0 and S[j] > key:
+            S[j + 1] = S[j]
+            j = j - 1
+        S[j + 1] = key
+    return S
